13f159a626a5c435c821106e3c04ba29d52f101d
Sai271828 <vb8@illinois.edu>
2025-02-09 18:13:18 -0500

    fixing bugs

--- boolnetanalyzer/load.py
+++ boolnetanalyzer/load.py
@@ -225,58 +225,59 @@ class BooleanNet():
 
         Parameters:
         -------------------
+        input_data : varies
           
-            The input data to load into the class. The structure of the input depends on the format type:
+        The input data to load into the class. The structure of the input depends on the format type:
 
-            **NCF-format**: Data structured according to the NCF format. Currently, this format is **not implemented** in the class.
+        **NCF-format**: Data structured according to the NCF format. Currently, this format is **not implemented** in the class.
 
-            **rtt-format**: Data structured using reduced truth tables.
+        **rtt-format**: Data structured using reduced truth tables.
 
-                This is a tuple consisting of two lists:
+            This is a tuple consisting of two lists:
 
-                1. The first list contains the reduced truth tables for each node. Each entry in this list represents the truth table for the corresponding node.
+            1. The first list contains the reduced truth tables for each node. Each entry in this list represents the truth table for the corresponding node.
 
-                2. The second list contains the regulators for each node. Each entry represents the nodes that regulate the output of the corresponding node.
+            2. The second list contains the regulators for each node. Each entry represents the nodes that regulate the output of the corresponding node.
 
-                Example:
-                    
-                    >>> ([[1, 1, 0, 0], [0, 1], [0, 0, 0, 0]], [[0, 2], [0], [1, 2]])
-                    
-                In this case, the truth tables are stored in the variable `self.truth_tables` and the regulators in the variable `self.regulators`.
+            Example:
+                
+                >>> ([[1, 1, 0, 0], [0, 1], [0, 0, 0, 0]], [[0, 2], [0], [1, 2]])
+                
+            In this case, the truth tables are stored in the variable `self.truth_tables` and the regulators in the variable `self.regulators`.
 
-            **text-format**: Data structured as text-based formulas.
+        **text-format**: Data structured as text-based formulas.
 
-                This is a list of tuples where each tuple consists of:
+            This is a list of tuples where each tuple consists of:
 
-                1. The first string representing the node (variable) name.
+            1. The first string representing the node (variable) name.
 
-                2. The second string representing the update formula associated with that node.
+            2. The second string representing the update formula associated with that node.
 
-                Example:
-                    
-                >>> [("x1", "x2 and not x1"), ("x2", "x1 or x3")]
-                    
-                In this case, the variables are stored in `self.variables`, the regulators are stored in `self.regulators` and the update formulas in `self.update_formulae`.
+            Example:
+                
+            >>> [("x1", "x2 and not x1"), ("x2", "x1 or x3")]
+                
+            In this case, the variables are stored in `self.variables`, the regulators are stored in `self.regulators` and the update formulas in `self.update_formulae`.
 
-                .. note::
-                    The constants found in the update formulas but not initially listed as nodes are added to both `self.variables` and `self.update_formulae`.
+            .. note::
+                The constants found in the update formulas but not initially listed as nodes are added to both `self.variables` and `self.update_formulae`.
 
         Raises:
         -----------------
-          ValueError: If the input format type is not one of the expected types ("NCF-format", "rtt-format", or "text-format"), a `ValueError` is raised.
+        ValueError: If the input format type is not one of the expected types ("NCF-format", "rtt-format", or "text-format"), a `ValueError` is raised.
 
         Examples:
         -----------------
             
-            >>> network = BooleanNet("rtt-format", 3)
-            >>> network.load(([[1, 1, 0, 0], [0, 1], [0, 0, 0, 0]], [[0, 2], [0], [1, 2]]))
-            >>> print(network.truth_tables), print(network.regulators)
-            [[1, 1, 0, 0], [0, 1], [0, 0, 0, 0]]
-            [[0, 2], [0], [1, 2]]
-            >>> network = BooleanNet("text-format", 2)
-            >>> network.load([("A", "A and B"), ("B", "not A")])
-            >>> print(network.update_formulae)
-            [('A', 'A and B'), ('B', 'not A')]
+        >>> network = BooleanNet("rtt-format", 3)
+        >>> network.load(([[1, 1, 0, 0], [0, 1], [0, 0, 0, 0]], [[0, 2], [0], [1, 2]]))
+        >>> print(network.truth_tables), print(network.regulators)
+        [[1, 1, 0, 0], [0, 1], [0, 0, 0, 0]]
+        [[0, 2], [0], [1, 2]]
+        >>> network = BooleanNet("text-format", 2)
+        >>> network.load([("A", "A and B"), ("B", "not A")])
+        >>> print(network.update_formulae)
+        [('A', 'A and B'), ('B', 'not A')]
             
         """
                 
